7c242636480a748803ffe81bfedf3b4103ad1c52
KtlTheBest <dbatyr09@gmail.com>
2018-10-06 18:53:22 +0600

    Added argument support

--- MyDirBuster.py
+++ MyDirBuster.py
@@ -5,10 +5,11 @@ Created on Sat Oct  6 17:48:03 2018
 @author: marik
 """
 
+import sys
 try:
-    import requests
+    import requests, getopt
 except ImportError:
-    print("This script requires 'requests' module! Please install it with pip!")
+    print("This script requires 'requests' and 'getopt' module! Please install it with pip!")
     raise Exception('exit')
 
 def addWordlist(filename):
@@ -17,26 +18,50 @@ def addWordlist(filename):
         return wordlist
     except:
         return None
+
+
+def checkUrl(url):
+    site = requests.get(url, allow_redirects=True)
+    return site.url
+
+def main(args):
         
-filename = "wordlist.txt"
-url = "https://www.google.com/"
-words = addWordlist(filename)
-
-result = open("result.txt", "w")
-
-if words == None:
-    print("Error opening the file! Check if the dictionary exists!")
-    raise Exception('exit')
-
-site = requests.get(url)
-result.write(str(site.status_code) + ' ' + url)
-result.write('\n')
-
-for word in words:
-    site = requests.get(url + word)
-    result.write(str(site.status_code) + ' ' + url + word)
+    try:
+        opt, vals = getopt.getopt(args, "hu:w:", ["help", "url=", "wordlist="])
+    except getopt.GetoptError:
+        print("use -h for help")
+        return
+    
+    words = None    
+    
+    for o, val in opt:
+        if o in ("-h", "--help"):
+            print(sys.argv[0] + " -u <url> -w <wordlist>")
+            return
+        if o in ("-u", "--url"):
+            url = val
+            url = checkUrl(url)
+        if o in ("-w", "--wordlist"):
+            words = addWordlist(val)
+    
+    result = open("result.txt", "w")
+    
+    if words == None:
+        print("Error opening the file! Check if the dictionary exists!")
+        raise Exception('exit')
+    
+    site = requests.get(url)
+    result.write(str(site.status_code) + ' ' + url)
     result.write('\n')
     
-result.close()
-words.close()
-    
+    for word in words:
+        site = requests.get(url + word)
+        result.write(str(site.status_code) + ' ' + url + word)
+        result.write('\n')
+        
+    result.close()
+    words.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
